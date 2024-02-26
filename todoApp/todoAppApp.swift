742b2be63431fb3fbd528cc2121608ef6104dc54
KosukeMiyake <kosuke.miyake@asia-quest.jp>
2024-02-26 23:44:58 +0900

    Initial Commit

---
+++ todoApp/todoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  todoApp
+//
+//  Created by kosuke.miyake on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+struct todoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
